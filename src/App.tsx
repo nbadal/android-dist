9cc031c299ac9e1f7bf81229b85f15614f791b44
Nick Badal <me@nbad.al>
2020-11-08 00:09:03 -0800

    Add percentage column and basic error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {CSSProperties, useEffect, useState} from 'react';
+import React, { CSSProperties, useEffect, useState } from 'react';
 import './App.css';
 
 type Distribution = {
@@ -31,6 +31,7 @@ function App() {
     const [refreshTime, setRefreshTime] = useState(Date.now)
     const [distributions, setDistributions] = useState<Distribution[]>([]);
     const [lastModified, setLastModified] = useState("");
+    const [fetchError, setFetchError] = useState<string | null>(null);
 
     useEffect(() => {
         const proxy = "https://cors-anywhere.herokuapp.com/"
@@ -40,36 +41,50 @@ function App() {
             .then(([data, modified]) => {
                 setDistributions(data);
                 setLastModified(modified);
+                setFetchError(null);
+            })
+            .catch(err => {
+                setDistributions([]);
+                setFetchError(err.message);
             })
     }, [refreshTime]);
 
     let percentage = 1.0
     return (
         <div className="App">
-            <div className="List">
-                <div className="Header"/>
-                <div className="Header">Platform Version</div>
-                <div className="Header">API Level</div>
-                <div className="Header">Cumulative Distribution</div>
-                {distributions.map((d, idx) => {
-                    const style: CSSProperties = {
-                        backgroundColor: colors[idx % colors.length]
-                    }
-                    const row = (
-                        <React.Fragment key={d.apiLevel}>
-                            <div className="version" style={style}>{d.version}</div>
-                            <div style={style}>{d.name}</div>
-                            <div className="apiLevel" style={style}>{d.apiLevel}</div>
-                            <div style={style}>{(percentage * 100).toFixed(1)}%</div>
-                        </React.Fragment>
-                    );
-                    percentage -= d.distributionPercentage;
-                    return row;
-                })}
-            </div>
-            <div className="Updated" onClick={() => setRefreshTime(Date.now)}>
-                Last Updated by Google: <span>{lastModified}</span>
-            </div>
+            {fetchError && (
+                <div>{fetchError}</div>
+            )}
+            {distributions.length > 0 && (
+                <>
+                    <div className="List">
+                        <div className="Header" />
+                        <div className="Header">Platform Version</div>
+                        <div className="Header">API Level</div>
+                        <div className="Header">% of Distribution</div>
+                        <div className="Header">Cumulative Distribution</div>
+                        {distributions.map((d, idx) => {
+                            const style: CSSProperties = {
+                                backgroundColor: colors[idx % colors.length]
+                            }
+                            const row = (
+                                <React.Fragment key={d.apiLevel}>
+                                    <div className="version" style={style}>{d.version}</div>
+                                    <div style={style}>{d.name}</div>
+                                    <div className="apiLevel" style={style}>{d.apiLevel}</div>
+                                    <div style={style}>{(d.distributionPercentage * 100).toFixed(1)}%</div>
+                                    <div style={style}>{(percentage * 100).toFixed(1)}%</div>
+                                </React.Fragment>
+                            );
+                            percentage -= d.distributionPercentage;
+                            return row;
+                        })}
+                    </div>
+                    <div className="Updated" onClick={() => setRefreshTime(Date.now)}>
+                        Last Updated by Google: <span>{lastModified}</span>
+                    </div>
+                </>
+            )}
         </div>
     );
 }
